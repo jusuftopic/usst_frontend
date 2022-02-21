97d41c1645889a11664ff9c7424f3adf37650b54
jusuftopic <jusuf.topic@smarter-software.com>
2022-02-21 08:53:18 +0100

    last time

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,7 +8,7 @@ const routes: Routes = [
   {path: 'gallery', loadChildren: () => import('./gallery-main/gallery-main.module').then(m => m.GalleryMainModule)},
   {path: 'blog', loadChildren: () => import('./blog-main/blog-main.module').then(m => m.BLogMainModule)},
   {path: 'contact', loadChildren: () => import('./contact/contact.module').then(m => m.ContactModule)},
-  {path: '', redirectTo: 'landing', pathMatch: 'full'}
+  {path: '', redirectTo: '/landing', pathMatch: 'full'}
 
 ];
 
