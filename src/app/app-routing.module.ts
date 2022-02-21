6c6d9d70580b56496d945b0dd55806a49301e3cc
jusuftopic <jusuf.topic@smarter-software.com>
2022-02-21 08:57:43 +0100

    last time

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {path: 'landing', loadChildren: () => import('./landing/landing.module').then(m => m.LandingModule)},
+  {path: '', loadChildren: () => import('./landing/landing.module').then(m => m.LandingModule)},
   {path: 'about', loadChildren: () => import('./about-main/about-main.module').then(m => m.AboutMainModule)},
   {path: 'courses', loadChildren: () => import('./courses/courses.module').then(m => m.CoursesModule)},
   {path: 'gallery', loadChildren: () => import('./gallery-main/gallery-main.module').then(m => m.GalleryMainModule)},
   {path: 'blog', loadChildren: () => import('./blog-main/blog-main.module').then(m => m.BLogMainModule)},
-  {path: 'contact', loadChildren: () => import('./contact/contact.module').then(m => m.ContactModule)},
-  {path: '', redirectTo: '/landing', pathMatch: 'full'}
+  {path: 'contact', loadChildren: () => import('./contact/contact.module').then(m => m.ContactModule)}
 
 ];
 
