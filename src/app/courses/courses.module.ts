5c9d8492df860eb1fd1c024b4fcd95e22e103a07
jusuftopic <jusuf.topic@smarter-software.com>
2022-03-06 10:19:30 +0100

    add admin login and dashboard

--- src/app/courses/courses.module.ts
+++ src/app/courses/courses.module.ts
@@ -5,11 +5,12 @@ import {CoursesRoutingModule} from './courses-routing.module';
 import {SharedModule} from '../shared/shared.module';
 import { CoursesIndividualComponent } from './courses-individual/courses-individual.component';
 import { CoursesGroupComponent } from './courses-group/courses-group.component';
+import {CoursesDetailComponent} from './courses-detail/courses-detail.component';
 
 
 
 @NgModule({
-  declarations: [CoursesComponent, CoursesIndividualComponent, CoursesGroupComponent],
+  declarations: [CoursesComponent, CoursesIndividualComponent, CoursesGroupComponent, CoursesDetailComponent],
   imports: [
     CommonModule,
     CoursesRoutingModule,
